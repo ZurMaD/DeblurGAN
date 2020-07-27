f76de84609c55bc967e881f9d9104e00b539dabc
ZurMaD <pablo.diazv@pucp.edu.pe>
2020-07-27 16:18:08 -0500

    Update image_folder.py

--- data/image_folder.py
+++ data/image_folder.py
@@ -23,6 +23,7 @@ def is_image_file(filename):
 
 def make_dataset(dir):
     images = []
+    print(str(dir))
     assert os.path.isdir(dir), '%s is not a valid directory' % dir
 
     for root, _, fnames in sorted(os.walk(dir)):
